d38359611dcd95f2db6a76d381488b9ee82e625a
manimike00 <manimike00@gmail.com>
2020-07-18 10:41:59 +0530

    add ps

--- terraform/aws/vpc/variables.tf
+++ terraform/aws/vpc/variables.tf
@@ -28,6 +28,12 @@ variable "public_subnet_cidrs" {
   default     = ["10.10.1.0/24"]
 }
 
+variable "private_subnet_cidrs" {
+  description = "A list of CIDR blocks to use for the private subnets."
+  type        = list(string)
+  default     = ["10.10.2.0/24"]
+}
+
 variable "azs" {
   description = "A list of availability zones names or ids in the region"
   type        = list(string)
