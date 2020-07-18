3a133cf29a2d905f9d5929e74754a8d9bf3a988b
manimike00 <manimike00@gmail.com>
2020-07-18 13:42:07 +0530

    add subnets

--- terraform/aws/vpc/variables.tf
+++ terraform/aws/vpc/variables.tf
@@ -25,13 +25,13 @@ variable "tags" {
 variable "public_subnet_cidrs" {
   description = "A list of CIDR blocks to use for the public subnets."
   type        = list(string)
-  default     = ["10.10.1.0/24,10.10.3.0/24"]
+  default     = ["10.10.1.0/24","10.10.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "A list of CIDR blocks to use for the private subnets."
   type        = list(string)
-  default     = ["10.10.2.0/24,10.10.4.0/24"]
+  default     = ["10.10.2.0/24","10.10.4.0/24"]
 }
 
 variable "azs" {
