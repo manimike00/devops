9b4e0e7ce18901d916c8533ddf9716c352fd8667
manimike00 <manimike00@gmail.com>
2021-01-22 03:14:58 +0530

    add binaries

--- k8s/eksctl/1.18/cloudshell.sh
+++ k8s/eksctl/1.18/cloudshell.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-echo "[TASK-1] Create Local Binary Directory"
-mkdir -p ~/.local/bin/kubectl
+echo "[TASK 1] Create Local Binary Directory"
+mkdir -p ~/.local/bin/
 
-echo "[TASK-2] Install kubectl binary"
-curl -LO https://dl.k8s.io/release/v1.20.0/bin/linux/amd64/kubectl
-mv ./kubectl ~/.local/bin/kubectl
+echo "[TASK 2] Install kubectl binary"
+sudo curl -LO https://dl.k8s.io/release/v1.20.0/bin/linux/amd64/kubectl >/dev/null 2>&1
+chown $USER:$USER kubectl
+chown 0755 kubectl
+mv ./kubectl ~/.local/bin/
+
+echo "[TASK 3] Install helm"
+wget https://get.helm.sh/helm-v3.5.0-linux-amd64.tar.gz >/dev/null 2>&1
+tar -zxf helm-v3.0.0-linux-amd64.tar.gz
+mv linux-amd64/helm ~/.local/bin/
+rm -rf linux-amd64
+
+echo "[TASK 4] Install eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ~/.local/bin/ >/dev/null 2>&1
+
+echo "[TASK 5] Set Path"
+export PATH=$PATH:~/.local/bin/
